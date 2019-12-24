5bd7a3ce544ca5f9bd3ac093e516feb6ee6474e8
koraynilay <koray.fra@gmail.com>
2019-12-24 11:03:20 +0000

    configs

---
+++ mapunmap_polybar.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if cat /tmp/polybar_hidden>/dev/null;then
+	xdo show -N Polybar
+	xdo show -a Polybar\ tray\ window
+	rm /tmp/polybar_hidden
+else
+	xdo hide -N Polybar
+	xdo hide -a Polybar\ tray\ window
+	touch /tmp/polybar_hidden
+fi
