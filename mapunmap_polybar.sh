68bf2f5a3f6d53dc696f0233eb88d6fcc727e8e0
koraynilay <koray.fra@gmail.com>
2020-02-23 21:46:22 +0000

    2020-02-23_21:46:22

--- mapunmap_polybar.sh
+++ mapunmap_polybar.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 if cat /tmp/polybar_hidden>/dev/null;then
-#	xdo show -N Polybar
-#	xdo show -a polybar-bottom_DVI-D-0
-#	xdo show -a Polybar\ tray\ window
+	xdo show -N Polybar
+	xdo show -a polybar-bottom_DVI-D-0
+	xdo show -a Polybar\ tray\ window
 	rm /tmp/polybar_hidden
 else
-#	xdo hide -N Polybar
-#	xdo hide -a polybar-bottom_DVI-D-0
-#	xdo hide -a Polybar\ tray\ window
+	xdo hide -N Polybar
+	xdo hide -a polybar-bottom_DVI-D-0
+	xdo hide -a Polybar\ tray\ window
 	touch /tmp/polybar_hidden
 fi
