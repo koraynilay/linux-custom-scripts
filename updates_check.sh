7ec6af4289a8a6ef88597c1cc5035293c25a2195
koraynilay <koray.fra@gmail.com>
2020-02-23 01:39:15 +0000

    2020-02-23_01:39:15

--- updates_check.sh
+++ updates_check.sh
@@ -3,7 +3,7 @@ while true;do
 	ups=`checkupdates`
 	if [[ -n $ups ]];then
 		act=`dunstify -a Updates -A "update,up" "Updates are available"`
-		! [[ -z $act ]] && termite --hold -e "printf \"Updates available:\n\n$ups\""
+		! [[ -z $act ]] && termite --hold -e "printf \"Updates available:\n\n$ups\";read"
 	#	termite --hold -e 'sh -c "printf \"Updates available:\n\n\"; checkupdates"'
 	fi
 	echo fatto
