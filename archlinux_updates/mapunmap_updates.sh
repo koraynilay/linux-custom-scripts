ee39db68f7ce43fd5e85cf58aa0cdd0dc9a4ccbb
koraynilay <koray.fra@gmail.com>
2020-02-27 12:56:48 +0000

    2020-02-27_12:56:48

--- archlinux_updates/mapunmap_updates.sh
+++ archlinux_updates/mapunmap_updates.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 id=`xdotool search --name "^archlinux_updates_script$"`
+pid=`ps x | awk '/\/bin\/sh \/usr\/bin\/updates_dialog_text/ {print $1}'`
 [[ -z $id ]] && updates_dialog &!
 if cat /tmp/archlinux_updates_script_hidden>/dev/null;then
 	xdotool windowmap $id
+	kill -USR1 $pid
 	rm /tmp/archlinux_updates_script_hidden
 else
 	xdotool windowunmap $id
