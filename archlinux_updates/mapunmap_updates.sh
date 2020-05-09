cd4f8a699885633f2b75814cbbdf09260628ad47
koraynilay <koray.fra@gmail.com>
2020-05-09 10:43:25 +0200

    2020-05-09_10:43:25

--- archlinux_updates/mapunmap_updates.sh
+++ archlinux_updates/mapunmap_updates.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
-id=`xdotool search --name "^archlinux_updates_script$"`
-pid=`ps x | awk '/\/bin\/sh \/usr\/bin\/updates_dialog_text/ {print $1}'`
-
-[[ -z $id ]] && updates_dialog &!
-
-if cat /tmp/archlinux_updates_script_hidden>/dev/null;then
-	xdotool windowmap $id
-#	kill -USR1 $pid
+if [ "$1" == "start" ];then
+	dialog_id=`xdotool search --name "^archlinux_updates_script$"`
+	if [ -n "$dialog_id" ];then
+		echo kill
+		xdotool windowkill $dialog_id
+	fi
+	echo start
+	termite -t "archlinux_updates_script" -e updates_dialog_text 2>/dev/null &!
 	rm /tmp/archlinux_updates_script_hidden
+	sleep 0.2
+	mapunmap_updates.sh
+	exit 0
 else
-	xdotool windowunmap $id
-	touch /tmp/archlinux_updates_script_hidden
+	echo mapunmap
+	id=`xdotool search --name "^archlinux_updates_script$"`
+	pid=`ps x | awk '/\/bin\/sh \/usr\/bin\/updates_dialog_text/ {print $1}'`
+	if cat /tmp/archlinux_updates_script_hidden>/dev/null;then
+		xdotool windowmap $id
+		rm /tmp/archlinux_updates_script_hidden
+	else
+		xdotool windowunmap $id
+		touch /tmp/archlinux_updates_script_hidden
+	fi
 fi
---
+++ win10vm.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+vmsfolder='/C/linux/vms'
+cd "$vmsfolder"
+qemu-system-x86_64 \
+	-enable-kvm \
+	-machine type=q35,accel=kvm \
+       	\
+	-device intel-iommu \
+	-cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time \
+	-m 10000 \
+	-smp 2 \
+	\
+	-net nic,model=virtio \
+	-net user,smb=$HOME/share_win \
+	-net tap,ifname=tap0,script=no,downscript=no \
+	\
+	-drive file=win10.qcow2,if=virtio,cache=none,aio=native,cache.direct=on \
+	\
+	-vga virtio -display sdl,gl=on \
+	\
+	-device ich9-intel-hda \
+	-audiodev pa,id=snd0 \
+	-device hda-output,audiodev=snd0
+	#-cdrom virtio-win-0.1.171.iso \
