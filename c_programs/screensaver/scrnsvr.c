7bb19c30829c4625e7e186810b7013be49361916
koraynilay <koray.fra@gmail.com>
2020-05-29 21:24:26 +0200

    2020-05-29_21:24:26

--- c_programs/screensaver/scrnsvr.c
+++ c_programs/screensaver/scrnsvr.c
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
 			sys=system(pgrep_lock)/256;
 			printf("sys = %d\n",sys);
 			if(sys == 1){
-				system("");
+				system(cmd_lock);
 				system(kill_sleep);
 				system(cmd_sleep);
 			}else{
