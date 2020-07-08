10c9cf6d0965748b040be3d3bc31b23f94d220e9
koraynilay <koray.fra@gmail.com>
2020-07-08 16:32:57 +0200

    2020-07-08_16:32:57

--- win10vm.sh
+++ win10vm.sh
@@ -9,11 +9,12 @@ qemu-system-x86_64 \
 	\
 	-enable-kvm \
 	-machine type=q35,accel=kvm \
-       	\
 	-device intel-iommu \
 	-cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time \
 	-m 6G \
 	-smp 2 \
+	\
+	-monitor unix:/tmp/monitor.sock,server,nowait \
 	\
 	-net nic,model=virtio \
 	-net user,smb=$HOME/share_win \
@@ -21,15 +22,22 @@ qemu-system-x86_64 \
 	\
 	-drive file=win10.qcow2,if=virtio,cache=none,aio=native,cache.direct=on \
 	\
-	-vga virtio  \
+	-vga virtio \
 	-display sdl,gl=on \
 	\
 	-device ich9-intel-hda \
 	-audiodev pa,id=snd0 \
-	-device hda-output,audiodev=snd0 #\
-	#-cdrom virtio-win-0.1.171.iso 
+	-device hda-output,audiodev=snd0 \
+	-cdrom virtio-win-0.1.171.iso 
 
 	#-device virtio-keyboard-pci \
 	#-device virtio-tablet-pci \
 	#-device virtio-gpu-pci \
 	#-device virtio-net-pci,netdev=net0 \
+
+
+	#-device virtio-serial-pci \
+	#-spice unix,addr=/tmp/vm_spice.socket,disable-ticketing \
+	#-device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 \
+	#-chardev spicevmc,id=spicechannel0,name=vdagent \
+	#-display spice-app \
