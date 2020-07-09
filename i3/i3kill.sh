6d2791575b9b75207ab22b09c2c814423832174e
koraynilay <koray.fra@gmail.com>
2020-07-09 10:27:52 +0200

    2020-07-09_10:27:52

--- i3/i3kill.sh
+++ i3/i3kill.sh
@@ -2,14 +2,35 @@
 winID=$(xdotool getactivewindow)
 winClass=$(xprop -id $winID WM_CLASS)
 winName=$(xprop -id $winID WM_NAME)
+sig=$(echo $1 | sed -e 's/[^0-9]//g')
+echo $sig
 if [[ $winClass = *"Steam"* ]]; then
-	xdotool windowunmap $(xdotool getactivewindow)
-	exit
+	xdotool windowunmap $winID
 elif [[ $winName = *"archlinux_updates_script"* ]]; then
-	xdotool windowunmap $(xdotool getactivewindow)
+	xdotool windowunmap $winID
 	touch /tmp/archlinux_updates_script_hidden
-	exit
 else
-	i3-msg kill
-	exit;
+	if [ $sig -gt 0 ];then
+		kill -$sig $(xdotool getwindowpid $winID)
+	else
+		i3-msg kill
+	fi
 fi
+exit
+
+
+
+#winID=$(xdotool getactivewindow)
+#winClass=$(xprop -id $winID WM_CLASS)
+#winName=$(xprop -id $winID WM_NAME)
+#if [[ $winClass = *"Steam"* ]]; then
+#	xdotool windowunmap $(xdotool getactivewindow)
+#	exit
+#elif [[ $winName = *"archlinux_updates_script"* ]]; then
+#	xdotool windowunmap $(xdotool getactivewindow)
+#	touch /tmp/archlinux_updates_script_hidden
+#	exit
+#else
+#	i3-msg kill
+#	exit;
+#fi
