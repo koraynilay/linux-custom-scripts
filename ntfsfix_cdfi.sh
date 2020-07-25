ecb3a2c5287cb48f55778fc36b0ba671e0daa480
koraynilay <koray.fra@gmail.com>
2020-07-25 23:58:16 +0200

    2020-07-25_23:58:16

--- ntfsfix_cdfi.sh
+++ ntfsfix_cdfi.sh
@@ -2,15 +2,20 @@ filter_regex="/\/C|\/D|\/F|\/I/"
 disks=$(lsblk --raw -o NAME,MOUNTPOINT,FSTYPE | awk "/sd[a-z][0-9]/ && \$2 ~ $filter_regex && \$3 ~ /ntfs/ {print \$1}")
 mntpt=$(lsblk --raw -o NAME,MOUNTPOINT,FSTYPE | awk "/sd[a-z][0-9]/ && \$2 ~ $filter_regex && \$3 ~ /ntfs/ {print \$2}")
 echo $disks
-for mnt in $mntpt; do
-	sudo umount $mnt
-done
-for hd in $disks; do
-	#sudo umount /dev/$hd
-	sudo ntfsfix /dev/$hd
-done
-printf "Remount all disks in /etc/fstab? [y/n]: "
-read ans
-if [ "$ans" = "y" ];then
-	sudo mount -a
+echo $mntpt
+printf "Continue? [y/n]: "
+read anc
+if [ "$anc" = "y" ];then
+	for mnt in $mntpt; do
+		sudo umount $mnt
+	done
+	for hd in $disks; do
+		#sudo umount /dev/$hd
+		sudo ntfsfix /dev/$hd
+	done
+	printf "Remount all disks in /etc/fstab? [y/n]: "
+	read ans
+	if [ "$ans" = "y" ];then
+		sudo mount -va
+	fi
 fi
