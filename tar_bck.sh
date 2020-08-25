5bc2dd379487b86173bf47bd470bde1a5326f88f
koraynilay <koray.fra@gmail.com>
2020-08-25 16:25:56 +0200

    2020-08-25_16:25:56

--- tar_bck.sh
+++ tar_bck.sh
@@ -4,15 +4,15 @@ folders=('/usr' '/var' '/opt' '/etc' '/root')
 exfolders=(${folders[@]} '/swpfl.sys')
 dest='/D/linux/tars'
 topts='--acls --xattrs -czpf'
-v=''
+v='-v'
 outtar=0
-while getopts voh opt;do
+while getopts noh opt;do
 	case $opt in
-		v)v='-v';;
+		n)v='';;
 		o)outtar=1;;
 		#?)echo -e "'$opt' Uknown option. Exiting"; exit 2;;
 		h)	echo -ne "Usage: $0 [opt]\n";
-			echo -ne "  -v\tbe verbose (print processed files)\n";
+			echo -ne "  -n\tdon't be verbose (dont't print processed files)\n";
 			echo -ne "  -o\toutput the tar output to $(eval echo ~$SUDO_USER)/tarbck_{folder_name}\n";
 			exit 0;;
 		?)exit 2;;
@@ -23,7 +23,7 @@ done
 for ((i=0;i<${#folders[@]};i++)) do
 	cf="${folders[i]}"
 	if [ $outtar -eq 1 ];then
-		o="> $(getent passwd $SUDO_USER | cut -d: -f6)/tarbck_out_${cf/\/}"
+		o="> $(eval echo ~$SUDO_USER)/tarbck_out_${cf/\/}"
 	fi
 	echo tar ${topts} ${v} ${dest}/${cf/\/}.tar.gz ${cf} ${o}
 done
