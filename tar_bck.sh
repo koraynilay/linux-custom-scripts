6df2d72940403d7e210cf34ac5f7a8783225120a
koraynilay <koray.fra@gmail.com>
2020-08-25 15:46:10 +0200

    2020-08-25_15:46:10

--- tar_bck.sh
+++ tar_bck.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
-folders=$('/usr' '/var' '/opt' '/etc' '/root')
-exfolders=$(${folders[@]} '/swpfl.sys')
+[[ $UID -ne 0 ]] && echo -e "This script needs root access. Exiting." && exit 1
+folders=('/usr' '/var' '/opt' '/etc' '/root')
+exfolders=(${folders[@]} '/swpfl.sys')
+dest='/D/linux/tars'
+topts='--acls --xattrs -czpvf'
+#echo ${folders[@]}
+#echo ${exfolders[@]}
+for ((i=0;i<${#folders[@]};i++)) do
+#	tar ${topts} ${dest}/${folders[i]/\/}.tar.gz ${folders[i]}
+done
+
+
+
+
 #tar --one-file-system --exclude=/etc --exclude=/opt --exclude=/root --exclude=/usr --exclude=/var --exclude=/swpfl.sys --acls --xattrs -czpvf /D/linux/tars/r.tar.gz /
-echo "$folders"
-echo "$exfolders"
