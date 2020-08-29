7b8625d4b5f18951e15a45d772bf3a4ecd883fe4
koraynilay <koray.fra@gmail.com>
2020-08-29 18:49:53 +0200

    2020-08-29_18:49:53

--- screencap_xclip.sh
+++ screencap_xclip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 slop_opts="-l -c 0.2,0,0.15,0.3 -b 1.5 -k" # -D"
 date=$(date +%d-%m-%Y_%H-%M-%S)
-xsel_gopts=""
+xclip_gopts=""
 recdesk_multi_opt="	--freq 44100 \
 			--fps 60 \
 			--no-wm-check \
@@ -15,21 +15,21 @@ case $1 in
 		filename="$HOME/Pictures/screens/${date}.png"
 		maim $filename \
 		&& dunstify -a maim "screenshot is $filename" \
-		&& xsel $xsel_gopts --clipboard "$filename"
+		&& xclip $xclip_gopts -t image/png -selection clipboard "$filename"
 	;;
 	shots)
 		filename="$HOME/Pictures/screens/${date}.png"
 		maim -s $slop_opts \
 		$filename \
 		&& dunstify -a maim "screenshot is $filename" \
-		&& xsel $xsel_gopts --clipboard "$filename"
+		&& xclip $xclip_gopts -t image/png -selection clipboard "$filename"
 	;;
 	cast)
 		filename="$HOME/Videos/screencasts/${date}.ogv"
 		recordmydesktop $recdesk_multi_opt \
 			-o $filename \
 			&& dunstify -a recordmydesktop "video is $filename" \
-			&& xsel $xsel_gopts --clipboard "$filename"
+			&& xclip $xclip_gopts -t video/ogg -selection clipboard "$filename"
 	;;
 	casts)
 		slop=$(slop $slop_opts -f "%x %y %w %h %g %i") || exit 1 #[1]
@@ -43,7 +43,7 @@ case $1 in
 			--height $H \
 			-o $filename \
 			&& dunstify -a recordmydesktop "video is $filename" \
-			&& xsel $xsel_gopts --clipboard "$filename"
+			&& xclip $xclip_gopts -t video/ogg -selection clipboard "$filename"
 			#--windowid $ID \ #for recordmydesktop
 	;;
 	*)printf "Usage: $0 [shot|shots|cast|casts]\n";exit 1;;
