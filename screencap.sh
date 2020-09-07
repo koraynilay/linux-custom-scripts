a71d9a9dbc8392fad910774bbf7e47eaf228045b
koraynilay <koray.fra@gmail.com>
2020-09-07 15:24:18 +0200

    2020-09-07_15:24:18

--- screencap.sh
+++ screencap.sh
@@ -4,12 +4,13 @@ date=$(date +%d-%m-%Y_%H-%M-%S)
 full_res=$(xrandr -q | awk '/\*/ {print $1}')
 xclip_gopts=""
 # -filter_complex and -map are from https://trac.ffmpeg.org/wiki/AudioChannelManipulation (Section: Merged audio channel)
-ffmepg_opts="ffmpeg -hwaccel_output_format cuda "
+fco="[1:a][2:a] amerge=inputs=2,pan=stereo|c0<c0+c2|c1<c1+c3[a]"
+ffmepg_opts="-hwaccel_output_format cuda "
 ffmepg_opts+="-f x11grab size_to_replace -i ${DISPLAY}offset_to_replace "
 ffmepg_opts+="-f pulse -i 2 -ac 2 "
 ffmepg_opts+="-f pulse -i 1 -ac 1 "
-ffmepg_opts+="-filter_complex '[1:a][2:a] amerge=inputs=2,pan=stereo|c0<c0+c2|c1<c1+c3[a]' "
-ffmepg_opts+="-map 0 -map '[a]' -map 1 -map 2 "
+ffmepg_opts+="-filter_complex $fco "
+ffmepg_opts+="-map 0 -map \"[a]\" -map 1 -map 2 "
 ffmepg_opts+="-c:v h264_nvenc -r:v 60 -b:v 10m -crf 0 "
 ffmepg_opts+="-c:a mp3 -r:a 44100 -b:a 320k "
 ffmepg_opts+="-preset fast "
@@ -31,7 +32,7 @@ case $1 in
 		filename=../cici.mkv
 		ffmepg_opts=${ffmepg_opts/size_to_replace/-s $full_res}
 		ffmepg_opts=${ffmepg_opts/offset_to_replace/}
-		$ffmepg_opts \
+		ffmpeg $ffmepg_opts \
 			$filename
 		#	&& dunstify -a ffmpeg "screencast is $filename" \
 		#	&& xclip $xclip_gopts -t video/ogg -selection clipboard "$filename"
