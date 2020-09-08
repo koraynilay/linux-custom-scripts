dc928ab2214b1378aca4a673403e681af88ef95c
koraynilay <koray.fra@gmail.com>
2020-09-08 22:43:36 +0200

    2020-09-08_22:43:36

--- screencap_maim.sh
+++ screencap_maim.sh
@@ -9,19 +9,15 @@ full_res=$(xrandr -q | awk '/\*/ {print $1}')
 xclip_opts=""
 
 #[3]
-ffmpeg_opts_video="-hwaccel_output_format cuda "
-ffmpeg_opts_video+="-f x11grab size_to_replace -i ${DISPLAY}offset_to_replace "
-ffmpeg_opts_video+="-f pulse -i 2 -ac 2 "
-ffmpeg_opts_video+="-f pulse -i 1 -ac 1 "
-ffmpeg_opts_video+="-filter_complex [1:a][2:a]amerge=inputs=2,pan=stereo|c0<c0+c2|c1<c1+c3[a] " #[2]
-ffmpeg_opts_video+="-map 0 -map [a] -map 1 -map 2 "
-ffmpeg_opts_video+="-c:v h264_nvenc -r:v 60 -b:v 10m -crf 0 "
-ffmpeg_opts_video+="-c:a mp3 -r:a 44100 -b:a 320k "
-ffmpeg_opts_video+="-preset fast "
-
-ffmpeg_opts_image="-f x11grab size_to_replace -i ${DISPLAY}offset_to_replace "
-ffmpeg_opts_image+="-vframes 1 -pix_fmt rgba "
-ffmpeg_opts_image+="-preset fast "
+ffmpeg_opts="-hwaccel_output_format cuda "
+ffmpeg_opts+="-f x11grab size_to_replace -i ${DISPLAY}offset_to_replace "
+ffmpeg_opts+="-f pulse -i 2 -ac 2 "
+ffmpeg_opts+="-f pulse -i 1 -ac 1 "
+ffmpeg_opts+="-filter_complex [1:a][2:a]amerge=inputs=2,pan=stereo|c0<c0+c2|c1<c1+c3[a] " #[2]
+ffmpeg_opts+="-map 0 -map [a] -map 1 -map 2 "
+ffmpeg_opts+="-c:v h264_nvenc -r:v 60 -b:v 10m -crf 0 "
+ffmpeg_opts+="-c:a mp3 -r:a 44100 -b:a 320k "
+ffmpeg_opts+="-preset fast "
 
 started_notif_time=200
 finished_notif_time=10000
@@ -31,30 +27,24 @@ resumed_notif_time=1000
 case $1 in
 	shot)
 		filename="$HOME/Pictures/screens/${date}.png"
-		ffmpeg_opts_image=${ffmpeg_opts_image/size_to_replace/-s $full_res}
-		ffmpeg_opts_image=${ffmpeg_opts_image/offset_to_replace/}
-		ffmpeg $ffmpeg_opts_image $filename \
-			&& dunstify -a ffmpeg "screenshot is $filename" -t $finished_notif_time \
+		maim $filename \
+			&& dunstify -a maim "screenshot is $filename" -t $finished_notif_time \
 			&& xclip $xclip_opts -t image/png -selection clipboard "$filename"
 	;;
 	shots)
-		slop=$(slop $slop_opts -f "%x %y %w %h %g %i") || exit 1 #[1]
 		filename="$HOME/Pictures/screens/${date}.png"
-		read -r X Y W H G ID < <(echo $slop) #[1]
-		ffmpeg_opts_image=${ffmpeg_opts_image/size_to_replace/-s ${W}x${H}}
-		ffmpeg_opts_image=${ffmpeg_opts_image/offset_to_replace/+${X},${Y}}
-		ffmpeg $ffmpeg_opts_image $filename \
-			&& dunstify -a ffmpeg "screenshot is $filename" -t $finished_notif_time \
+		maim -s $slop_opts $filename \
+			&& dunstify -a maim "screenshot is $filename" -t $finished_notif_time \
 			&& xclip $xclip_opts -t image/png -selection clipboard "$filename"
 	;;
 	cast)
 		filename="$HOME/Videos/screencasts/${date}.mkv"
-		ffmpeg_opts_video=${ffmpeg_opts_video/size_to_replace/-s $full_res}
-		ffmpeg_opts_video=${ffmpeg_opts_video/offset_to_replace/}
+		ffmpeg_opts=${ffmpeg_opts/size_to_replace/-s $full_res}
+		ffmpeg_opts=${ffmpeg_opts/offset_to_replace/}
 		echo "$filename" > "$lastfile"
 
 		dunstify -a screencap.sh "rec started" -t $started_notif_time
-		ffmpeg $ffmpeg_opts_video $filename
+		ffmpeg $ffmpeg_opts $filename
 		ffmpeg_exit_code=$?
 		echo $ffmpeg_exit_code
 		if [ $ffmpeg_exit_code -eq 255 ] || [ $ffmpeg_exit_code -eq 0 ];then
@@ -69,12 +59,12 @@ case $1 in
 		slop=$(slop $slop_opts -f "%x %y %w %h %g %i") || exit 1 #[1]
 		filename="$HOME/Videos/screencasts/${date}_select.mkv"
 		read -r X Y W H G ID < <(echo $slop) #[1]
-		ffmpeg_opts_video=${ffmpeg_opts_video/size_to_replace/-s ${W}x${H}}
-		ffmpeg_opts_video=${ffmpeg_opts_video/offset_to_replace/+${X},${Y}}
+		ffmpeg_opts=${ffmpeg_opts/size_to_replace/-s ${W}x${H}}
+		ffmpeg_opts=${ffmpeg_opts/offset_to_replace/+$X,$Y}
 		echo -e "$filename\n$X\n$Y\n$W\n$H" > "$lastfile"
 
 		dunstify -a screencap.sh "rec started" -t $started_notif_time
-		ffmpeg $ffmpeg_opts_video $filename
+		ffmpeg $ffmpeg_opts $filename
 		if [ $ffmpeg_exit_code -eq 255 ] || [ $ffmpeg_exit_code -eq 0 ];then
 			dunstify -a ffmpeg "screencast is $filename" -t $finished_notif_time
 			xclip $xclip_opts -t video/ogg -selection clipboard "$filename"
@@ -115,15 +105,15 @@ case $1 in
 				echo $fnl $X $Y $W $H
 				IFS=$defIFS
 				if [[ "$fnl" =~ .*_select.* ]];then
-					ffmpeg_opts_video=${ffmpeg_opts_video/size_to_replace/-s ${W}x${H}}
-					ffmpeg_opts_video=${ffmpeg_opts_video/offset_to_replace/+${X},${Y}}
+					ffmpeg_opts=${ffmpeg_opts/size_to_replace/-s ${W}x${H}}
+					ffmpeg_opts=${ffmpeg_opts/offset_to_replace/+${X},${Y}}
 				else 
-					ffmpeg_opts_video=${ffmpeg_opts_video/size_to_replace/-s $full_res}
-					ffmpeg_opts_video=${ffmpeg_opts_video/offset_to_replace/}
+					ffmpeg_opts=${ffmpeg_opts/size_to_replace/-s $full_res}
+					ffmpeg_opts=${ffmpeg_opts/offset_to_replace/}
 				fi
 				filename="${fnl}.tmp_to_concat.mkv"
 				dunstify -a screencap.sh "rec started" -t $started_notif_time
-				ffmpeg $ffmpeg_opts_video $filename
+				ffmpeg $ffmpeg_opts $filename
 				ffmpeg_exit_code=$?
 				echo $ffmpeg_exit_code
 				if [ $ffmpeg_exit_code -eq 255 ] || [ $ffmpeg_exit_code -eq 0 ];then
