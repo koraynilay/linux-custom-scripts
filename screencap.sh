d63d4c8c990c058e55683bf141a49d33ddef91ab
koraynilay <koray.fra@gmail.com>
2020-09-19 16:01:53 +0200

    2020-09-19_16:01:53

--- screencap.sh
+++ screencap.sh
@@ -2,6 +2,9 @@
 lastfile="$HOME/.screencapsh"
 lockfile="$HOME/.screencapsh.lck"
 
+video_ext="mkv"
+image_ext="png"
+
 slop_opts="-l -c 0.2,0,0.15,0.3 -b 1.5 -k" # -D"
 date=$(date +%d-%m-%Y_%H-%M-%S)
 
@@ -30,7 +33,7 @@ resumed_notif_time=1000
 
 case $1 in
 	shot)
-		filename="$HOME/Pictures/screens/${date}.png"
+		filename="$HOME/Pictures/screens/${date}.${image_ext}"
 		ffmpeg_opts_image=${ffmpeg_opts_image/size_to_replace/-s $full_res}
 		ffmpeg_opts_image=${ffmpeg_opts_image/offset_to_replace/}
 		ffmpeg $ffmpeg_opts_image $filename \
@@ -39,7 +42,7 @@ case $1 in
 	;;
 	shots)
 		slop=$(slop $slop_opts -f "%x %y %w %h %g %i") || exit 1 #[1]
-		filename="$HOME/Pictures/screens/${date}.png"
+		filename="$HOME/Pictures/screens/${date}.${image_ext}"
 		read -r X Y W H G ID < <(echo $slop) #[1]
 		ffmpeg_opts_image=${ffmpeg_opts_image/size_to_replace/-s ${W}x${H}}
 		ffmpeg_opts_image=${ffmpeg_opts_image/offset_to_replace/+${X},${Y}}
@@ -48,7 +51,7 @@ case $1 in
 			&& xclip $xclip_opts -t image/png -selection clipboard "$filename"
 	;;
 	cast)
-		filename="$HOME/Videos/screencasts/${date}.mkv"
+		filename="$HOME/Videos/screencasts/${date}.${video_ext}"
 		ffmpeg_opts_video=${ffmpeg_opts_video/size_to_replace/-s $full_res}
 		ffmpeg_opts_video=${ffmpeg_opts_video/offset_to_replace/}
 		echo "$filename" > "$lastfile"
@@ -67,7 +70,7 @@ case $1 in
 	;;
 	casts)
 		slop=$(slop $slop_opts -f "%x %y %w %h %g %i") || exit 1 #[1]
-		filename="$HOME/Videos/screencasts/${date}_select.mkv"
+		filename="$HOME/Videos/screencasts/${date}_select.${video_ext}"
 		read -r X Y W H G ID < <(echo $slop) #[1]
 		ffmpeg_opts_video=${ffmpeg_opts_video/size_to_replace/-s ${W}x${H}}
 		ffmpeg_opts_video=${ffmpeg_opts_video/offset_to_replace/+${X},${Y}}
@@ -121,13 +124,13 @@ case $1 in
 					ffmpeg_opts_video=${ffmpeg_opts_video/size_to_replace/-s $full_res}
 					ffmpeg_opts_video=${ffmpeg_opts_video/offset_to_replace/}
 				fi
-				filename="${fnl}.tmp_to_concat.mkv"
+				filename="${fnl}.tmp_to_concat.${video_ext}"
 				dunstify -a screencap.sh "rec started" -t $started_notif_time
 				ffmpeg $ffmpeg_opts_video $filename
 				ffmpeg_exit_code=$?
 				echo $ffmpeg_exit_code
 				if [ $ffmpeg_exit_code -eq 255 ] || [ $ffmpeg_exit_code -eq 0 ];then
-					out_temp="${fnl}_concat.mkv"
+					out_temp="${fnl}_concat.${video_ext}"
 					ffmpeg -f concat -safe 0 -i <(echo -e "file '$fnl'\nfile '$filename'") -c copy "$out_temp"
 					mv -vf "$out_temp" "$fnl"
 					rm "$filename"
