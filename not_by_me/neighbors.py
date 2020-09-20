3d726fa3d5a2f5d004fcee43da6146af2678c422
koraynilay <koray.fra@gmail.com>
2020-09-20 20:58:53 +0200

    2020-09-20_20:58:53

---
+++ not_by_me/neighbors.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+""" or just use `arp`/`ip neigh`/`ip n` - by u/koraynilay """
+
+"""List all hosts with their IP adress of the current network."""
+
+import os
+
+out = os.popen('ip neigh').read().splitlines()
+for i, line in enumerate(out, start=1):
+    ip = line.split(' ')[0]
+    h = os.popen('host {}'.format(ip)).read()
+    hostname = h.split(' ')[-1]
+    print("{:>3}: {} ({})".format(i, hostname.strip(), ip))
