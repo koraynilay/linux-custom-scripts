ec3255b688103e8893b3e9b76f1e15a5b50378d1
koraynilay <koray.fra@gmail.com>
2020-10-06 16:15:38 +0200

    2020-10-06_16:15:38

--- game_byid_steam.sh
+++ game_byid_steam.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 v=0
 f=0
-while getopts hvdf: opt;do
+while getopts hvd:f: opt;do
 	case $opt in
 		f)folder=$OPTARG;f=1;;
 		v)v=1;;
-		h)echo -e "Error";exit 1;;
+		h)echo -e "-h help\n-v verbose (show id)\n-f subfolders of this folder are steam ids\n-d with -f, add id";exit 1;;
 		d)id=$OPTARG;;
 	esac
 done
-if [ $# -gt 3 ];then
+if [ $# -gt 2 ];then
 	if [ $f -eq 1 ];then
-		for iid in $folder/*;do 
-			id=$(basename $iid)
+		for current in $folder/*;do 
+			id=$(basename $current)
 			if [ $v -eq 1 ];then
 				echo $id: $(curl -s https://store.steampowered.com/app/$id | grep -Po '(?<=<div class="apphub_AppName">).*(?=</div>)')
 			else
