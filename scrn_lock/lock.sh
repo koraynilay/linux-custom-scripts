0e06ef8e8154e29f2dde5feba4469cbc437b6a80
koraynilay <koray.fra@gmail.com>
2020-10-13 15:03:31 +0200

    2020-10-13_15:03:31

--- scrn_lock/lock.sh
+++ scrn_lock/lock.sh
@@ -5,7 +5,7 @@ dark_pic="$HOME/Pictures/wallpapers/sweet/landscape_sweet_dark_blur.png"
 #light_pic="$home/Pictures/wallpapers/wave/landscape_wave_blur.png"
 #dark_pic="$home/Pictures/wallpapers/wave/landscape_wave_4_blur.png"
 font='Tw Cen MT'
-if [ `date +%H` -lt 20 -a `date +%H` -gt 8 ];then
+if [ `date +%H` -lt 20 -a `date +%H` -ge 8 ];then
 	pic=$light_pic
 else
 	pic=$dark_pic
