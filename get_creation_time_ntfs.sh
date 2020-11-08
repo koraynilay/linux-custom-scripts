1c9a46a6d30995873b666b3131d233eaa890b396
koraynilay <koray.fra@gmail.com>
2020-11-08 11:59:22 +0100

    2020-11-08_11:59:22

--- get_creation_time_ntfs.sh
+++ get_creation_time_ntfs.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 date_format="$2"
+date_format=""
 time=`getfattr --only-values -n system.ntfs_crtime_be "$1" 2>/dev/null | perl -MPOSIX -0777 -ne '$t = unpack("Q>"); print $t/10000000-11644473600'`
 #epoch=`echo -n $a | perl -MPOSIX -0777 -ne '$t = unpack("Q>"); print $t/10000000-11644473600'`
 #date --date="@$epoch"
