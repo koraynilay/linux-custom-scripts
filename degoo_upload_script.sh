70ed192d07f6975795c9a5e9f7f40ddf814fd2a1
koraynilay <koray.fra@gmail.com>
2021-04-17 20:30:56 +0200

    2021-04-17_20:30:56

--- degoo_upload_script.sh
+++ degoo_upload_script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-echo=0;
+echo pwd:`pwd`. Are you sure?
+echo=1;
+read res
+if [ "$res" == "yes" ]; then
+	echo=0;
+fi
 foldername="`basename $PWD`_degoo";
 if [ $echo -eq 1 ];then
 	echo="echo";
@@ -7,6 +12,6 @@ else
 	echo="";
 fi
 for a in *; do
-	$echo mkdir -p "${foldername}/${a}.folder";
+	$echo mkdir -vp "${foldername}/${a}.folder";
 	$echo split --verbose -b 500M -d -a 3 "$a" "${foldername}/${a}.folder/${a}.";
 done
