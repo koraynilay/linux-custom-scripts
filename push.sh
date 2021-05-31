8b78f0c9864130af35893a21c2929eb4025da7d1
koraynilay <koray.fra@gmail.com>
2021-06-01 00:44:33 +0200

    2021-06-01_00:44:33 - lastncmfat port to zsh - for reorder_ost()

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@ git status
 git add .
 git status
 #sleep 1
-git commit -m "$(date +%Y-%m-%d_%H:%M:%S)"
+git commit -m "$(date +%Y-%m-%d_%H:%M:%S) - lastncmfat port to zsh - for reorder_ost()"
 #sleep 1
 git push
 notify-send -a GitHub "changes in $PWD pushed"
