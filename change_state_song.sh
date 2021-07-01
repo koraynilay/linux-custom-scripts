f7656deabcdbe53bc3653a2ff4da70f946468b87
koraynilay <koray.fra@gmail.com>
2021-07-01 21:17:23 +0200

    2021-07-01_21:17:23

--- change_state_song.sh
+++ change_state_song.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 lyrdir="$HOME/.lyrics"
-case $1 in
-	next) next;;
-	prev) prev;;
-	*)mpc $1;;
-esac
 check_dir(){
 	if ! [ -d "$lyrdir" ];then
 		echo "'$lyrdir' doesn't exists"
@@ -15,7 +10,7 @@ next(){
 	check_dir
 	IFS=$'\n'
 	filename="${lyrdir}/$(mpc current).txt"
-	if [ -f "$filename"];then
+	if [ -f "$filename" ];then
 		lstart=$(($(grep -n Tracklist "${filename}" | cut -f1 -d:)+1))
 		lend=$(($(grep -n Endtr "${filename}" | cut -f1 -d:)-1))
 		for line in $(sed -n -e "${lstart},${lend}p" "${filename}");do
@@ -23,14 +18,35 @@ next(){
 			mc=$(echo $timec | awk -F: '{print $(NF-1)}')
 			sc=$(echo $timec | awk -F: '{print $(NF-0)}')
 			timef=$(echo $line | awk '{print $1}')
+			#echo $mc $sc
 			#echo $timef
-			mf=$(echo $timef | awk -F: '{print $(NF-1)}')
-			sf=$(echo $timef | awk -F: '{print $(NF-0)}')
-			#echo $mf $sf
-			if [[ $s -le  ]];then
-
+			mf=$(echo $timef | awk -F: '{s=$NF;m=$(NF-1);if($(NF-2) != $0)m+=($(NF-2)*60);print m}')
+			sf=$(echo $timef | awk -F: '{s=$NF;m=$(NF-1);if($(NF-2) != $0)m+=($(NF-2)*60);print s}')
+			echo $mf $sf
+			mc=$((10#$mc))
+			sc=$((10#$sc))
+			mf=$((10#$mf))
+			sf=$((10#$sf))
+			if [[ $mc -lt $mf ]];then
+				echo ciao $timef
+				mpc seek $timef
+				songstate_change_notif
+				exit
+			#elif [[ $mc -eq $mf ]];then
+			#	if [[ $sc -le $sf ]];then
+			#		echo ciao $timef
+			#		mpc seek $timef
+			#		songstate_change_notif
+			#		exit
+			#	fi
 			fi
-			lasttc=$time
-		fi
+			lasttc=$timef #time start, for prev
+		done
 	fi
 }
+
+case $1 in
+	next) next;;
+	prev) prev;;
+	*)mpc $1;;
+esac
