fb83c1b044b354d85b767f7bafea28bb73d78532
koraynilay <koray.fra@gmail.com>
2021-08-08 02:14:43 +0200

    2021-08-08_02:14:43

--- get_creation_time_ntfs.sh
+++ get_creation_time_ntfs.sh
@@ -4,7 +4,8 @@ date_format=""
 time=`getfattr --only-values -n system.ntfs_crtime_be "$1" 2>/dev/null | perl -MPOSIX -0777 -ne '$t = unpack("Q>"); print $t/10000000-11644473600'`
 #epoch=`echo -n $a | perl -MPOSIX -0777 -ne '$t = unpack("Q>"); print $t/10000000-11644473600'`
 #date --date="@$epoch"
-date --date="@$time" $date_format
+echo -ne "$(date --date="@$time" $date_format)\t"
+echo $1
 
 #time=`getfattr --only-values -n system.ntfs_crtime_be $@`
 #for a in $time;do
--- yadmpush.sh
+++ yadmpush.sh
@@ -24,26 +24,49 @@ pkg_list_ver() {
 }
 list_films_animes() {
 	cd /F/free_ck/films
-	paste <(/bin/ls -tr | xargs -d'\n' du -s | cut -f1) <(/bin/ls -tr | xargs -d'\n' du -s | cut -f2- | xargs -d'\n' ls -d --color=always -U) > ~/filmsanimes_list_zips.txt
+	paste <(/bin/ls -tr | xargs -d'\n' du -s | cut -f1) <(/bin/ls -tr | xargs -d'\n' du -s | cut -f2- | xargs -d'\n' ls -d --color=always -U) > ~/filmsanimes_list.txt
+	cd /F/free_ck/anime
+	echo >> ~/filmsanimes_list.txt
+	paste <(/bin/ls -tr | xargs -d'\n' du -s | cut -f1) <(/bin/ls -tr | xargs -d'\n' du -s | cut -f2- | xargs -d'\n' ls -d --color=always -U) >> ~/filmsanimes_list.txt
 	#alias lsdt="paste <(/bin/ls -tr | xargs -d'\n' du -s | cut -f1) <(/bin/ls -tr | xargs -d'\n' du -s | cut -f2- | xargs -d'\n' ls -d --color=always -U)"
-	#lsdt > ~/filmsanimes_list_zips.txt
+	#lsdt > ~/filmsanimes_list.txt
 }
 
-pkg_list
-pkg_list_ver
-list_films_animes
-cp -v /I/Raccolte/music_files.txt ~/ 
-yadm status
-sleep 1
-yadm add -u
-yadm add -v ~/.lyrics
-yadm add -v ~/.mpd/playlists/
-yadm add -v ~/.mpd/database
-yadm status
-yadm commit -m "$(date +'%Y-%m-%d %H:%M:%S')"
-yadm push
+m_function() {
+	m="git --git-dir=$HOME/.config/dotfiles-minimal/dotfiles-minimal.git --work-tree=$HOME -c status.showUntrackedFiles=no"
+	pkg_list
+	pkg_list_ver
+	list_films_animes
+	cp -v /I/Raccolte/music_files.txt ~/ 
+	$m status
+	sleep 1
+	$m add -u
+	$m add -v ~/.lyrics
+	$m add -v ~/.mpd/playlists/
+	$m add -v ~/.mpd/database
+	$m status
+	$m commit -m "$(date +'%Y-%m-%d %H:%M:%S')"
+	$m push
+}
+yadm_function() {
+	pkg_list
+	pkg_list_ver
+	list_films_animes
+	cp -v /I/Raccolte/music_files.txt ~/ 
+	yadm status
+	sleep 1
+	yadm add -u
+	yadm add -v ~/.lyrics
+	yadm add -v ~/.mpd/playlists/
+	yadm add -v ~/.mpd/database
+	yadm status
+	yadm commit -m "$(date +'%Y-%m-%d %H:%M:%S')"
+	yadm push
+}
+yadm_function
 read -p "copyq? [y/n]:" ans
 if [ "$ans" = "y" ];then
 	~/linux-custom-scripts/copyq_push.sh
 fi
+m_function
 #f=()
