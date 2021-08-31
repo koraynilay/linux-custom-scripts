8cd3d35576e3305d7e0e25ef8fb602714919378f
koraynilay <koray.fra@gmail.com>
2021-09-01 00:55:27 +0200

    2021-09-01_00:55:27

--- dsstream.sh
+++ dsstream.sh
@@ -2,17 +2,17 @@
 ns=/tmp/dsstream-null-sink # null sink
 lm=/tmp/dsstream-loopback-mic # loopback mic
 la=/tmp/dsstream-loopback-apps # loopback apps/audio
-case $1 in
-	on|start)dsstreamon;;
-	off|stop)dsstreamoff;;
-esac
-dsstream(){
+dsstreamon(){
 	pactl load-module module-null-sink sink_name=dsstream sink_properties=device.description=dsstream > "$ns"
 	pactl load-module module-loopback source=PulseEffects_mic.monitor sink=dsstream > "$lm"
 	pactl load-module module-loopback source=PulseEffects_apps.monitor sink=dsstream > "$la"
 }
-dsstream(){
+dsstreamoff(){
 	pactl unload-module $(cat "$ns")
 	pactl unload-module $(cat "$lm")
 	pactl unload-module $(cat "$la")
 }
+case $1 in
+	on|start)dsstreamon;;
+	off|stop)dsstreamoff;;
+esac
