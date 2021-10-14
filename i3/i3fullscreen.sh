ff37eb37d824fa62e64c43532eb21e1b805d8756
koraynilay <koray.fra@gmail.com>
2021-10-14 19:36:05 +0200

    2021-10-14_19:36:05

---
+++ i3/i3fullscreen.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+winID=$(xdotool getactivewindow)
+winClass=$(xprop -id $winID WM_CLASS)
+winName=$(xprop -id $winID WM_NAME)
+echo $sig
+if [[ $winClass = *"google-chrome"* ]]; then
+	xdotool key --window $winID F11
+	exit $?
+else
+	i3-msg fullscreen toggle
+	exit $?
+fi
+
+
+
+#winID=$(xdotool getactivewindow)
+#winClass=$(xprop -id $winID WM_CLASS)
+#winName=$(xprop -id $winID WM_NAME)
+#if [[ $winClass = *"Steam"* ]]; then
+#	xdotool windowunmap $(xdotool getactivewindow)
+#	exit
+#elif [[ $winName = *"archlinux_updates_script"* ]]; then
+#	xdotool windowunmap $(xdotool getactivewindow)
+#	touch /tmp/archlinux_updates_script_hidden
+#	exit
+#else
+#	i3-msg kill
+#	exit;
+#fi
