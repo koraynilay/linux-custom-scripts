f7b681ed5aa27441b2a4615651999cdb4d7d5ebb
koraynilay <koray.fra@gmail.com>
2021-10-17 15:21:46 +0200

    2021-10-17_15:21:46

---
+++ swap_update.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+swapfile=/swpfl.sys
+[[ $UID -ne 0 ]] && echo -e "This script needs root access. Exiting." && exit 1
+if [ "$1" = "off" ];then
+	swapoff "$swapfile" -v
+	rm "$swapfile"
+elif [ "$1" = "on" ];then
+	fallocate -l 16G "$swapfile"
+	chmod 0600 "$swapfile"
+	mkswap "$swapfile"
+	swapon "$swapfile" -v
+else
+	echo -e "Usage: $0 [on|off]"
+fi
