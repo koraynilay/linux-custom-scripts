fcb6a2273949f462fbde92b3d7c60c589fa2d1e0
koraynilay <koray.fra@gmail.com>
2022-04-10 21:40:09 +0200

    2022-04-10_21:40:09

--- clean_pkgs.sh
+++ clean_pkgs.sh
@@ -9,10 +9,10 @@ ask() {
 	return 1
 }
 IFS=$'\n'
-pkgs=$(expac "%m %n" -l'\n' -Q -H M $target | sed s/MiB//g | sort -nr)
+pkgs=$(expac "%m %n" -l'\n' -Q -H M $target | sed s/MiB//g | sort -n)
 for pkg in $pkgs;do
 	pkg_name=$(echo $pkg | awk '{print $2}')
 	pkg_size=$(echo $pkg | awk '{print $1}')
-	printf "%5b\t%b\t%-30b\t%s " $pkg_size MiB $pkg_name 'info?'
-	ask && pacman -Qi $pkg_name && ask "remove $pkg_name ($pkg_size)?" && sudo pacman -Rns $pkg_name
+	#printf "%5b\t%b\t%-30b\t%s " $pkg_size MiB $pkg_name 'info?'
+	pacman -Qi $pkg_name | grep -Ee '^Nome\s' -e '^Descrizione\s' -e '^Richiesto da\s' -e '^Opzionale per\s' -e '^URL\s' && ask "remove $pkg_name ($pkg_size)?" && sudo pacman -Rns $pkg_name
 done
