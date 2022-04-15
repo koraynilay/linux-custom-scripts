1eca4443406f03ccde1023ea8903a6099b7dd16f
koraynilay <koray.fra@gmail.com>
2022-04-15 22:19:24 +0200

    2022-04-15_22:19:24

--- push.sh
+++ push.sh
@@ -5,5 +5,6 @@ git status
 #sleep 1
 git commit -m "$(date +%Y-%m-%d_%H:%M:%S)"
 #sleep 1
+notify-send -a GitHub "pushing changes in $PWD..."
 git push
 notify-send -a GitHub "changes in $PWD pushed"
