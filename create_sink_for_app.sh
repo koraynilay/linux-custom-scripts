5682aa8f1b06c3ef81cd0aec3e1d9af369235fed
koraynilay <koray.fra@gmail.com>
2022-04-22 22:07:02 +0200

    2022-04-22_22:07:02

--- create_sink_for_app.sh
+++ create_sink_for_app.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 names=("$@")
 sinks_file="/tmp/custom_app_sinks"
+common_sink_name="common_sink"
 for n in "${names[@]}";do
 	defsink="$(pactl get-default-sink)"
 	echo loading sink for "$n"...
-	sink="$(pactl load-module module-null-sink sink_name="$(echo $n | tr ' ' '_')" sink_properties=device.description="$(echo $n | tr ' ' '_')")"
+	common_sink="$(pactl load-module module-null-sink sink_name=$common_sink_name sink_properties=device.description=$common_sink_name)"
+	sn="$(echo $n | tr ' ' '_')" #sn = sink name
+	sink="$(pactl load-module module-null-sink sink_name="$sn" sink_properties=device.description="$sn")"
+	lp="$(pactl load-module module-loopback source="$sn.monitor" sink=$common_sink_name)" # lp = loopback
 	pactl set-default-sink "$defsink"
 	echo loaded sink $sink for "$n"...
 	notify-send -a "$0" "added sink $sink named '$n'"
 	printf "%d;" $sink >> "$sinks_file"
 	printf "%s\n" "$n" >> "$sinks_file"
+	printf "%d;" $common_sink >> "$sinks_file"
+	printf "%s\n" "$n - common_sink" >> "$sinks_file"
+	printf "%d;" $lp >> "$sinks_file"
+	printf "%s\n" "$n - loopback" >> "$sinks_file"
+	printf "\n" >> "$sinks_file"
 done
