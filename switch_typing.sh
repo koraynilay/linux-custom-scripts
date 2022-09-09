78696b732e3ef3a7361e7d16d6f7b8ec4b47848c
koraynilay <koray.fra@gmail.com>
2022-09-09 20:50:15 +0200

    2022-09-09_20:50:15

--- switch_typing.sh
+++ switch_typing.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
-backspace_code=22
-alt_L_code=64
-backspace=$(xmodmap -pke | /bin/grep -P "[^0-9]$backspace_code[^0-9]" | cut -f2 -d'=')
-alt=$(xmodmap -pke | /bin/grep -P "[^0-9]$alt_L_code[^0-9]" | cut -f2 -d'=')
+xmodmap_file="$HOME/.Xmodmap"
+switch_key() {
+	code1=$1
+	code2=$2
+	string1=$(xmodmap -pke | /bin/grep -P "[^0-9]$code1[^0-9]" | cut -f2 -d'=')
+	string2=$(xmodmap -pke | /bin/grep -P "[^0-9]$code2[^0-9]" | cut -f2 -d'=')
+
+	xmodmap -e "keycode $code1 = $string2"
+	xmodmap -e "keycode $code2 = $string1"
+}
 
 if [ "$1" == "on" ];then
-	xmodmap -e "keycode $backspace_code = $alt_L_code"
-	xmodmap -e "keycode $alt_L_code = $backspace"
+	#22 = BackSpace, 134 = Super_R, 64 = Alt_L, 36 = Return
+	switch_key 22 64
+	switch_key 134 36
 else
-	xmodmap ~/.Xmodmap
+	xmodmap $xmodmap_file
 fi
