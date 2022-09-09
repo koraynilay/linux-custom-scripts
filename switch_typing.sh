b969b02ae96884b1a557c51d0a74a46679d96527
koraynilay <koray.fra@gmail.com>
2022-09-09 19:06:31 +0200

    2022-09-09_19:06:31

--- switch_typing.sh
+++ switch_typing.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-backspace=$(xmodmap -pke | /bin/grep -F 'BackSpace')
-backspace_string=$(echo $backspace | cut -f2 -d'=')
-backspace_code=$(echo $backspace | cut -f1 -d'=')
-alt=$(xmodmap -pke | /bin/grep -F 'BackSpace')
+backspace_code=22
+alt_L_code=64
+backspace=$(xmodmap -pke | /bin/grep -P "[^0-9]$backspace_code[^0-9]" | cut -f2 -d'=')
+alt=$(xmodmap -pke | /bin/grep -P "[^0-9]$alt_L_code[^0-9]" | cut -f2 -d'=')
+
+if [ "$1" == "on" ];then
+	xmodmap -e "keycode $backspace_code = $alt_L_code"
+	xmodmap -e "keycode $alt_L_code = $backspace"
+else
+	xmodmap ~/.Xmodmap
+fi
