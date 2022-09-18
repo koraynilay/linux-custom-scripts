f7ecbe38b04d0c479a31bd4c13eb690fef33cd88
koraynilay <koray.fra@gmail.com>
2022-09-18 22:06:35 +0200

    2022-09-18_22:06:35

--- osu-lazer.sh
+++ osu-lazer.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 #export SDL_VIDEO_X11_FORCE_EGL=true
+# from https://github.com/ppy/osu/issues/11800#issuecomment-878649469
+export COMPlus_GCGen0MaxBudget=600000
+# from https://github.com/ppy/osu/issues/11800#issuecomment-786541035
+export COMPlus_TieredCompilation=0
+export COMPlus_TC_QuickJit=0
+export COMPlus_TC_QuickJitForLoops=0
+
 osu-lazer 2>&1 > ~/osu_log.log
 cd /C/osu
 ./push.sh
