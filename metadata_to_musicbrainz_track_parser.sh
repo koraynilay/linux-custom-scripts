082b378326eb786e0b4e6480df1f74eabf6d4059
koraynilay <koray.fra@gmail.com>
2024-02-03 03:38:03 +0100

    2024-02-03_03:38:03

--- metadata_to_musicbrainz_track_parser.sh
+++ metadata_to_musicbrainz_track_parser.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 for a in *;do
-	track="$(ffprobe "$a" 2>&1 | grep -P '[^a-zA-Z_]'track | awk '{print $NF}' | sed 's/\/0//g')"
-	title="$(ffprobe "$a" 2>&1 | grep -P '[^a-zA-Z_]'title | cut -f2- -d':')"
-	artist="$(ffprobe "$a" 2>&1 | grep -P '[^a-zA-Z_]'artist | cut -f2 -d':')"
-	duration="$(ffprobe "$a" 2>&1 | grep Duration | cut -f2- -d':' | sed 's/\.[0-9][0-9].*//g' | tr -d ' ')"
+	track="$(ffprobe "$a" 2>&1 | grep --ignore-case -P '[^a-zA-Z_]\strack' | awk '{print $NF}' | sed 's/\/0//g')"
+	title="$(ffprobe "$a" 2>&1 | grep --ignore-case -P '[^a-zA-Z_]\stitle' | cut -f2- -d':')"
+	artist="$(ffprobe "$a" 2>&1 | grep --ignore-case -P '[^a-zA-Z_]\sartist' | cut -f2 -d':')"
+	duration="$(ffprobe "$a" 2>&1 | grep --ignore-case duration | cut -f2- -d':' | sed 's/\.[0-9][0-9].*//g' | tr -d ' ')"
 	echo "$track.$title -$artist ($duration)"
 done
