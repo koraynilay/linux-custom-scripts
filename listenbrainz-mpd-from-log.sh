765d90c7bef36c3371711b51d7202d80912709ed
koraynilay <koray.fra@gmail.com>
2024-04-28 02:05:50 +0200

    2024-04-28_02-05-50

--- listenbrainz-mpd-from-log.sh
+++ listenbrainz-mpd-from-log.sh
@@ -4,7 +4,8 @@
 url="https://api.listenbrainz.org/1/submit-listens"
 music_dir="/I/Raccolte/Musica"
 info_func() {
-	ffprobe -v quiet -show_format -of json $1 | jq $2
+	ret=$(ffprobe -v quiet -show_format -of json $1 | jq $2)
+	printf '%s' "$ret"
 }
 
 # read from ~/.mpd/log from a certain time to another (quicker but less clean: use line numbers)
@@ -20,10 +21,25 @@ for song in $to_add;do
 		continue
 	fi
 	filename=${filename//\"/}
-	info_func $music_dir/$filename ".format.tags.TITLE"
+	json_cur=$(info_func $music_dir/$filename ".format")
 
 	now=$(date +%s)
 	listened_at=$(date -d "$month $day $time" +%s)
+	printf '%s' "$json_cur"
+	format=$(jq '.format_name' <<< $json_cur)
+	if [ "$format" = "mp3" ];then
+		title=$(jq '.tags.title' <<< $json_cur)
+		artist=$(jq '.tags.artist' <<< $json_cur)
+		album=$(jq '.tags.album' <<< $json_cur)
+
+		track_mbid="$(jq '.tags."MusicBrainz Release Track Id"' <<< $json_cur)"
+		recording_mbid="$(jq '.tags.""' <<< $json_cur)"
+		release_mbid="$(jq '.tags.""' <<< $json_cur)"
+	elif [ "$format" = "flac" ];then
+		title=$(jq '.tags.TITLE' <<< $json_cur)
+		artist=$(jq '.tags.ARTIST' <<< $json_cur)
+		album=$(jq '.tags.ALBUM' <<< $json_cur)
+	fi
 
 	json="
 	{
@@ -43,11 +59,81 @@ for song in $to_add;do
 	}
 	"
 	printf '%s' "$json"
+
+	json="
+	{
+	  \"inserted_at\": $now,
+	  \"listened_at\": $listened_at,
+	  \"track_metadata\": {
+	    \"additional_info\": {
+	      \"artist_mbids\": [
+		\"$artist_mbid\"
+	      ],
+	      \"duration_ms\": $duration,
+	      \"media_player\": \"MPD\",
+	      \"recording_mbid\": \"$recording_mbid\",
+	      \"release_mbid\": \"$release_mbid\",
+	      \"submission_client\": \"listenbrainz-mpd-from-logs.sh\",
+	      \"track_mbid\": \"$track_mbid\",
+	      \"tracknumber\": \"$track_number\"
+	    },
+	    \"artist_name\": \"$artist\",
+	    \"track_name\": \"$title\"
+	    \"release_name\": \"$album\",
+	  },
+	  \"user_name\": \"koraynilay\"
+	}
+	"
+
+	printf '%s' "$json"
 done
 
 
 exit 1
 curl -X POST $url -H "Authorization: token $(<$TOKEN_FILE)" -d "$json"
+
+#{
+#  "inserted_at": 1714236481,
+#  "listened_at": 1714236392,
+#  "recording_msid": "3f65f2e5-8f3a-4467-9ac2-6a52e9052b4e",
+#  "track_metadata": {
+#    "additional_info": {
+#      "artist_mbids": [
+#        "a9d3a905-9a5b-4c84-829a-2f8fedf3e513"
+#      ],
+#      "duration_ms": 176440,
+#      "media_player": "MPD",
+#      "recording_mbid": "8ed6cb7e-8f37-43da-8f05-0817e477be94",
+#      "recording_msid": "3f65f2e5-8f3a-4467-9ac2-6a52e9052b4e",
+#      "release_mbid": "21a925c9-1fc1-45e7-a4c0-9424242e8282",
+#      "submission_client": "listenbrainz-mpd",
+#      "submission_client_version": "2.3.4",
+#      "track_mbid": "442995ef-c57f-34fe-a06c-c98d03d5535d",
+#      "tracknumber": "10"
+#    },
+#    "artist_name": "Shintaro Jinbo",
+#    "mbid_mapping": {
+#      "artist_mbids": [
+#        "a9d3a905-9a5b-4c84-829a-2f8fedf3e513"
+#      ],
+#      "artists": [
+#        {
+#          "artist_credit_name": "神保伸太郎",
+#          "artist_mbid": "a9d3a905-9a5b-4c84-829a-2f8fedf3e513",
+#          "join_phrase": ""
+#        }
+#      ],
+#      "caa_id": 6079247992,
+#      "caa_release_mbid": "21a925c9-1fc1-45e7-a4c0-9424242e8282",
+#      "recording_mbid": "8ed6cb7e-8f37-43da-8f05-0817e477be94",
+#      "recording_name": "SCARE SHADOW",
+#      "release_mbid": "21a925c9-1fc1-45e7-a4c0-9424242e8282"
+#    },
+#    "release_name": "Saya no Uta Original Soundtrack",
+#    "track_name": "SCARE SHADOW"
+#  },
+#  "user_name": "koraynilay"
+#}
 
 #{
 #  "inserted_at": 1714186556,
