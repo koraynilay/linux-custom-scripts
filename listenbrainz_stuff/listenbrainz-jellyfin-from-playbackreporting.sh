285034e67e2653a7f80084ab03595899454a573c
koraynilay <koray.fra@gmail.com>
2024-05-07 01:17:32 +0200

    2024-05-07_01-17-32 - listenbrainz-jellyfin-from-playbackreporting.sh: now it gets the filenames right

--- listenbrainz_stuff/listenbrainz-jellyfin-from-playbackreporting.sh
+++ listenbrainz_stuff/listenbrainz-jellyfin-from-playbackreporting.sh
@@ -8,21 +8,22 @@ alias json_metadata_cmd='ffprobe -v quiet -show_format -of json'
 file="$HOME/jellyfin_listenbrainz/playback_reporting.db.sql_output_edited.json"
 
 len=$(jq ". | length" < $file)
-for i in `seq 71 72`;do
-#for i in `seq 0 $len`;do
+#for i in `seq 279 279`;do
+for i in `seq 0 $len`;do
 	json=$(jq ".[$i]" < $file)
+
+	echo -ne "$i\r"
+	sleep 0.05
 
 	itemtype=$(jq '.ItemType // empty' <<< $json)
 	if [ "$itemtype" != "Audio" ];then
 		continue
 	fi
 	itemname=$(jq '.ItemName // empty' <<< $json)
-	set -x
-	eval $(perl -ne "/^([^-]*(?:[^ ]* -[^ ]|[^ ]- [^ ]*|[^ ]*[^ ]-[^ ][^ ]*)*[^-]*)(?-1)* - (.*) \\(((?:[^()]*\\([^()]*\\)[^()]*)|(?:[^()]*))\\)$/;
-			print \"artist=\'\".\$1.\"\'\n\";
-			print  \"title=\'\".\$2.\"\'\n\";
-			print  \"album=\'\".\$3.\"\'\n\";" <<< "$itemname")
-	set +x
+	eval $(perl -ne '/^([^-]*(?:[^ ]* -[^ ]|[^ ]- [^ ]*|[^ ]*[^ ]-[^ ][^ ]*)*[^-]*)(?-1)* - (.*) \(((?:[^()]*\([^()]*\)[^()]*)|(?:[^()]*))\)$/;
+			print "artist=\"".($1 =~ s/"/\\"/rg)."\"\n";
+			print  "title=\"".($2 =~ s/"/\\"/rg)."\"\n";
+			print  "album=\"".($3 =~ s/"/\\"/rg)."\"\n";' <<< "$itemname")
 	if [ "$artist" = "Not Known" ] || [ "$artist" = "Artista sconosciuto" ];then
 		artist=""
 	fi
@@ -40,6 +41,7 @@ for i in `seq 71 72`;do
 #		echo [$i] at:$artist tt:$title ab:$album tn:$tracknumber rmbid:$recording_mbid
 #		echo $filename
 	fi
+	echo $filename
 
 	artist=""
 	title=""
