37b3ddb6728889661c3e08bd46b4335949386707
koraynilay <koray.fra@gmail.com>
2024-05-31 00:44:25 +0200

    2024-05-31_00:44:25

--- listenbrainz_stuff/rename_not_found.sh
+++ listenbrainz_stuff/rename_not_found.sh
@@ -5,4 +5,4 @@ mpdfile=$HOME/mpd_log_for_listenbrainz-v2-epoch
 while read -r cf1 <&3 && read -r cf2 <&4; do
 	echo perl -pi -e "s{\Q\"$cf1\"}{\"$cf2\"}g" $mpdfile
 	perl -pi -e "s{\Q\"$cf1\"}{\"$cf2\"}g" $mpdfile
-done 3<nntt 4<nntt2
+done 3<nntt6 4<nntt62
