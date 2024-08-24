8161fa7611964cdd80bf0f230a50a1535dfabb5c
koraynilay <koray.fra@gmail.com>
2024-08-24 06:33:53 +0200

    2024-08-24_06:33:53

---
+++ listenbrainz_stuff/picard_add_by_api_search.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#IFS=$'\n'
+if [[ $1 = "-d" ]];then
+	set -x
+fi
+for folder in "$@"; do
+	v=$(grep -Po '(?<={).*(?=})' <<< "$folder")
+
+	if [[ -z $v ]];then
+		continue
+	fi
+	
+	echo -n "adding $v to picard"
+	key="catno"
+	value="$v"
+	query="$key:$value"
+	mbid="$(curl --get --silent --data-urlencode "query=$query" https://musicbrainz.org/ws/2/release)"
+	echo $mbid
+	mbid="$(xq -r '.metadata."release-list".release."@id"' <<< "$mbid")"
+	#echo "$mbid"
+	curl http://127.0.0.1:8000/openalbum\?id\="$mbid"
+	echo
+	sleep 0.3
+done
