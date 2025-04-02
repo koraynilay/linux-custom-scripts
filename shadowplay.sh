8303626c463639c7ec095f6b1cea7a5f54d58920
koraynilay <koray.fra@gmail.com>
2025-04-02 03:55:48 +0200

    2025-04-02_03-55-48 - shadowplay.sh: update gpu-screen-recorder mode and add record command (tho it can't be used while also having replay mode activated

--- shadowplay.sh
+++ shadowplay.sh
@@ -6,10 +6,18 @@ time_sec=300
 folder="/home/koraynilay/Videos/screencasts"
 fps=60
 quality="ultra"
-mode="screen-direct" # screen only records 1 monitor, direct all of them
+mode="screen-direct" # screen only records 1 monitor, screen-direct all of them
+recpidfile="/tmp/gpu-screen-recorder-recordpidfile"
 case $1 in
 	start)
 		gpu-screen-recorder -w "$mode" -c "$ext" -f "$fps" -a "$sound" -a "$mic" -q "$quality" -r "$time_sec" -k h265 -o "$folder" &!
+		;;
+	record)
+		gpu-screen-recorder -w "$mode" -c "$ext" -f "$fps" -a "$sound" -a "$mic" -q "$quality" -k h265 -o "$folder/$(date +%Y-%m-%d_%H-%M-%S).$ext" &!
+		echo $! > "$recpidfile"
+		;;
+	stop-record)
+		kill -SIGINT $(<"$recpidfile")
 		;;
 	save-replay)
 		killall -SIGUSR1 gpu-screen-recorder
